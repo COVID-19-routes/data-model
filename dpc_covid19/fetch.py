910898d855b212e489b68bd03a7ceac21e514783
Stefano Miccoli <stefano.miccoli@polimi.it>
2021-09-26 17:46:18 +0200

    Reduce module fetch to the bare minimum

--- dpc_covid19/fetch.py
+++ dpc_covid19/fetch.py
@@ -3,53 +3,25 @@ functions to get data from DPC GitHub repository at
 https://github.com/pcm-dpc/COVID-19
 """
 
-import re
-
 import pandas as pd
-import requests
 
-# GitHub REST API v3
-API_HEADERS = {"Accept": "application/vnd.github.v3+json"}
-URL_API = (
-    "https://api.github.com/repos/{owner}/{repo}/contents/{path}?ref={branch}"
-).format
-URL_REG = URL_API(
-    owner="pcm-dpc", repo="COVID-19", path="dati-regioni", branch="master"
+# GitHub raw.githubusercontent.com
+_URL_TEMPLATE = (
+    "https://raw.githubusercontent.com/{owner}/{repo}/{branch}/{path}".format
 )
-URL_PRO = URL_API(
+URL_REG = _URL_TEMPLATE(
     owner="pcm-dpc",
     repo="COVID-19",
-    path="dati-province",
     branch="master",
+    path="dati-regioni/dpc-covid19-ita-regioni.csv",
+)
+URL_PRO = _URL_TEMPLATE(
+    owner="pcm-dpc",
+    repo="COVID-19",
+    branch="master",
+    path="dati-province/dpc-covid19-ita-province.csv",
 )
 
-DROP_COLS = ["stato", "lat", "long"]
-DATA_COLS_REG = [
-    "ricoverati_con_sintomi",
-    "terapia_intensiva",
-    "totale_ospedalizzati",
-    "isolamento_domiciliare",
-    "totale_positivi",
-    "variazione_totale_positivi",
-    "nuovi_positivi",
-    "dimessi_guariti",
-    "deceduti",
-    "totale_casi",
-    "tamponi",
-    "casi_testati",
-]
-TEXT_COLS_REG = ["note_it", "note_en"]
-
-INDEX_COLS_REG = ["data", "codice_regione", "codice_provincia"]
-INDEX_COLS_PRO = ["data", "codice_provincia"]
-
-# regex of province and regioni pathnames
-# names for files for each date
-# re_pro = re.compile(r"dpc-covid19-ita-province-\d{8}.csv")
-# re_reg = re.compile(r"dpc-covid19-ita-regioni-\d{8}.csv")
-# name of cumulative file
-re_pro = re.compile(r"dpc-covid19-ita-province.csv")
-re_reg = re.compile(r"dpc-covid19-ita-regioni.csv")
 
 __all__ = ["province", "regioni"]
 
@@ -84,49 +56,13 @@ def _fix_nuts_code(frame, unique_col):
             frame.loc[mask, c] = frame.loc[mask, unique_col].map(mapping)
 
 
-def _read_regioni(path):
-    frame = _read_csv(path)
-    _fix_nuts_code(frame, "codice_regione")
-    assert frame.codice_nuts_1.notna().all()
-    assert frame.codice_nuts_2.notna().all()
-
-    return frame
-
-
-def _read_province(path):
-    frame = _read_csv(path)
-    _fix_nuts_code(frame, "codice_provincia")
-    assert frame.codice_nuts_1.notna().all()
-    assert frame.codice_nuts_2.notna().all()
-
-    return frame
-
-
 def regioni():
-    req_api = requests.get(URL_REG, headers=API_HEADERS)
-    urls = []
-    for i in req_api.json():
-        if re_reg.fullmatch(i["name"]):
-            urls.append(i["download_url"])
-    if len(urls) == 0:
-        return None
-    regioni = _read_regioni(urls[0])
-    for i in urls[1:]:
-        regioni = regioni.append(_read_regioni(i), ignore_index=True)
-
-    return regioni
+    frame = _read_csv(URL_REG)
+    _fix_nuts_code(frame, "codice_regione")
+    return frame
 
 
 def province():
-    req_api = requests.get(URL_PRO, headers=API_HEADERS)
-    urls = []
-    for i in req_api.json():
-        if re_pro.fullmatch(i["name"]):
-            urls.append(i["download_url"])
-    if len(urls) == 0:
-        return None
-    province = _read_province(urls[0])
-    for i in urls[1:]:
-        province = province.append(_read_province(i), ignore_index=True)
-
-    return province
+    frame = _read_csv(URL_PRO)
+    _fix_nuts_code(frame, "codice_provincia")
+    return frame
--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,6 +1,7 @@
 import pytest
 from pandas.testing import assert_series_equal
-from dpc_covid19 import code, fetch
+
+from dpc_covid19 import fetch
 
 
 def test_NA(province):
@@ -10,31 +11,3 @@ def test_NA(province):
         province.denominazione_provincia == "Napoli",
         check_names=False,
     )
-
-
-def test_code(province, regioni):
-    assert (
-        regioni.index.get_level_values("codice_regione")
-        .isin(code.denominazione_regione.keys())
-        .all()
-    ), "spurious codice_regione in fetch.regioni"
-    assert (
-        province.index.get_level_values("codice_provincia")
-        .isin(code.denominazione_provincia.keys())
-        .all()
-    ), "spurious codice_provincia in fetch.province"
-    assert province.codice_regione.isin(
-        code.denominazione_regione.keys()
-    ).all(), "spurious codice_regione in fetch.province"
-
-
-def test_trbz(regioni):
-
-    with pytest.raises(KeyError):
-        regioni.xs(key=0, level="codice_provincia").xs(
-            key=4, level="codice_regione"
-        )
-
-
-def test_validate(regioni, province):
-    fetch.validate(regioni, province)
